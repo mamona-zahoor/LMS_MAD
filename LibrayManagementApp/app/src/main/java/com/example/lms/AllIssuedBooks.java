507e0704e3d65e4b7f38f4ad742eaa1baf5671fc
mamona-zahoor <uetian997@gmail.com>
2019-12-25 23:49:48 -0800

    Made changes to view issued books

--- LibrayManagementApp/app/src/main/java/com/example/lms/AllIssuedBooks.java
+++ LibrayManagementApp/app/src/main/java/com/example/lms/AllIssuedBooks.java
@@ -2,7 +2,6 @@ package com.example.lms;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
